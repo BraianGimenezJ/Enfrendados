780ad14094f791355c6f55abeb65e2aa38b47fcc
BraianGimenezJ <GimenezBraian@outlook.es>
2025-06-24 23:04:00 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 void MostrarTitulo();
 void MostrarMenu (string& jugador1, string& jugador2);
+void ComienzaPartidajugador1(string& jugador1, string& jugador2);
 int TirarDado6caras();
+int TirarDado12caras();
 
 
 int main (){
 
  srand(time(nullptr));
- string nombreJugador1;
- string nombreJugador2;
+ string jugador1;
+ string jugador2;
+
 
  MostrarTitulo();
 
- MostrarMenu(nombreJugador1, nombreJugador2);
+ MostrarMenu(jugador1, jugador2);
+
+ ComienzaPartidajugador1(jugador1, jugador2);
 
 
   return 0;
