f68af3e162c9da6ae90cfc99a3e494442b589d5a
BraianGimenezJ <GimenezBraian@outlook.es>
2025-06-24 23:03:44 -0300

    Update funcionespuntoh.cpp

--- funcionespuntoh.cpp
+++ funcionespuntoh.cpp
@@ -1,6 +1,7 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 void MostrarTitulo();
@@ -8,6 +9,7 @@ void MostrarMenu(string& jugador1, string& jugador2);
 string PreguntoNombreJugador1();
 string PreguntoNombreJugador2();
 int TirarDado6caras();
-
+int TirarDado12caras();
+void ComienzaPartidajugador1(string& jugador1, string& jugador2);
 
 #endif // FUNCIONES_H_INCLUDED
