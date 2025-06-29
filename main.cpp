ddbf8c5674e200506702ca05b7037c7b06e10674
ariel7600 <73848722+ariel7600@users.noreply.github.com>
2025-06-29 14:34:04 -0300

    Update main.cpp
    fer version 2.0

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
+#include "funciones.h"
 using namespace std;
 
 void MostrarTitulo();
 void MostrarMenu (string& jugador1, string& jugador2);
-void ComienzaPartidajugador1(string& jugador1, string& jugador2);
+void Rondas(string& jugador1, string& jugador2, int n, int &puntaje1, int &puntaje2);
 int TirarDado6caras();
 int TirarDado12caras();
+void victoriaAutomatica(int dados, int puntaje, bool j);
 
 
 int main (){
@@ -15,13 +18,8 @@ int main (){
  string jugador1;
  string jugador2;
 
-
  MostrarTitulo();
-
  MostrarMenu(jugador1, jugador2);
-
- ComienzaPartidajugador1(jugador1, jugador2);
-
 
   return 0;
 }
